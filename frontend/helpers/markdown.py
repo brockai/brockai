33e4107e2d24a104e00465b49dfea24cfb1ba58c
brockai <bclayton403@gmail.com>
2023-12-12 13:27:02 -0700

    bump

--- frontend/helpers/markdown.py
+++ frontend/helpers/markdown.py
@@ -1,8 +1,9 @@
 sidebar_footer_logo = """
 <div class="top-space">Docs & Jupyter Notebooks</div>
+<div class="left-space top-left-space">☁️&nbsp;&nbsp;&nbsp;<a href="https://github.com/brockai/brockai/wiki/Cloud-Deployment" target="_blank">Cloud Deployment</a></div>
 <div class="left-space top-left-space">🤗&nbsp;&nbsp;&nbsp;<a href="https://github.com/brockai/brockai/wiki/🤗-Huggingface" target="_blank">Huggingface</a></div>
 <div class="left-space top-left-space"><img src="app/static/openai.png" alt="openai" width="18">&nbsp;&nbsp;&nbsp;<a href="https://github.com/brockai/brockai/wiki/OpenAI" target="_blank">OpenAI</a></div>
-<div class="left-space top-left-space">👑&nbsp;&nbsp;&nbsp;<a href="https://github.com/brockai/brockai/wiki/deepset-(Haystack)" target="_blank">Haystack (deepset)</a></div>
+<div class="left-space top-left-space">👑&nbsp;&nbsp;&nbsp;<a href="https://github.com/brockai/brockai/wiki/deepset-(Haystack)" target="_blank">deepset (Haystack)</a></div>
 <div><br></div>
 <div class="footer">
 <a href="https://github.com/brockai" target="_blank">
