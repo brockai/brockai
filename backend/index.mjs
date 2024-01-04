20b0e5b8965d3bfcc7dd9aa352254c4262edab49
brockai <bclayton403@gmail.com>
2024-01-04 06:58:50 -0700

    bump

--- backend/index.mjs
+++ backend/index.mjs
@@ -1,25 +1,12 @@
 import express from 'express';
-import session from 'express-session';
-import Keycloak from 'keycloak-connect';
-
-import dotenv from 'dotenv';
-dotenv.config();
 
 import cors from 'cors';
-import vectorRouter from './routes/index.mjs';
+import platformRouter from './routes/index.mjs';
 const app = express();
-
-app.use(
-  session({
-    secret: process.env.BEARER_TOKEN_SECRET,
-    resave: false,
-    saveUninitialized: true,
-  })
-);
 
 app.use(express.json());
 app.options('*', cors());
 app.use(cors());
-// app.use('/api',vectorRouter);
+app.use('/api', platformRouter);
  
 app.listen(9000, () => console.log('brockai api listening on port 9000!'));
