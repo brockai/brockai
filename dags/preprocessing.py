cebc1c78e241d0433d3a5e43a3e9526e918fff1f
brock <bclayton403@gmail.com>
2024-02-22 15:02:02 -0700

    bump

--- dags/preprocessing.py
+++ dags/preprocessing.py
@@ -1,7 +1,26 @@
+"""
+Example DAG demonstrating the usage of labels with different branches.
+"""
+import pendulum
 
-import streamlit as st
-st.set_page_config(layout="wide")
-# import logging
-# logging.basicConfig(format="%(levelname)s - %(name)s -  %(message)s", level=logging.WARNING)
-# logging.getLogger("haystack").setLevel(logging.INFO)
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.utils.edgemodifier import Label
 
+with DAG(
+    "example_branch_labels",
+    schedule_interval="@daily",
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+) as dag:
+    ingest = EmptyOperator(task_id="ingest")
+    analyse = EmptyOperator(task_id="analyze")
+    check = EmptyOperator(task_id="check_integrity")
+    describe = EmptyOperator(task_id="describe_integrity")
+    error = EmptyOperator(task_id="email_error")
+    save = EmptyOperator(task_id="save")
+    report = EmptyOperator(task_id="report")
+
+    ingest >> analyse >> check
+    check >> Label("No errors") >> save >> report
+    check >> Label("Errors found") >> describe >> error >> report
