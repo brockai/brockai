9e60e10e641230afe793c9c079cac30058cc263b
brock <bclayton403@gmail.com>
2024-02-22 11:28:54 -0700

    preprocessing dag

--- dags/platform_preprocessing.py
+++ dags/platform_preprocessing.py
@@ -1,51 +1,23 @@
+import pendulum
 
-import streamlit as st
-st.set_page_config(layout="wide")
-# import logging
-# logging.basicConfig(format="%(levelname)s - %(name)s -  %(message)s", level=logging.WARNING)
-# logging.getLogger("haystack").setLevel(logging.INFO)
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.utils.edgemodifier import Label
 
-import pandas as pd
+with DAG(
+    "example_branch_labels",
+    schedule_interval="@daily",
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+) as dag:
+    ingest = EmptyOperator(task_id="ingest")
+    analyse = EmptyOperator(task_id="analyze")
+    check = EmptyOperator(task_id="check_integrity")
+    describe = EmptyOperator(task_id="describe_integrity")
+    error = EmptyOperator(task_id="email_error")
+    save = EmptyOperator(task_id="save")
+    report = EmptyOperator(task_id="report")
 
-from helpers.markdown import sidebar_links_footer, app_header
-from helpers.errorcheck import elasticsearch_health
-
-st.title("👑 deepset File Upload & Search 🧪")
-
-with open('styles.css') as f:
-    st.sidebar.markdown(
-        f'<style>{f.read()}</style>'
-        +app_header
-        +sidebar_links_footer
-        , unsafe_allow_html=True
-    )
-# from deepset.piplines import deepset_indexsearch
-# pipelineIndex = deepset_indexsearch()
-
-isHealthy = elasticsearch_health()
-
-# if isHealthy:
-#     from helpers.piplines import deepset_indexsearch
-#     pipelineIndex = deepset_indexsearch()
-# else:
-#     st.error(':disappointed: Sorry, the system is not available right now')    
-    
-uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True, disabled=not isHealthy)
-for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
-    st.write("filename:", uploaded_file.name)
-    # st.write(bytes_data)
-      
-question = st.text_input(
-    "Search for compliancy by part no, supplier, manufacturer and more", 
-    placeholder="Enter part no, supplier, manufacturer",
-    max_chars=100, disabled=not uploaded_files)
-
-# run_pressed = st.button("Run", disabled=not uploaded_files)
-
-# if run_pressed and isHealthy:
-#     from deepset.piplines import deepset_prediction_pipeline
-#     pipelineQuery = deepset_prediction_pipeline(question)
-#     answer_df = pd.DataFrame(pipelineQuery["answers"])
-#     answer_df
-
+    ingest >> analyse >> check
+    check >> Label("No errors") >> save >> report
+    check >> Label("Errors found") >> describe >> error >> report
